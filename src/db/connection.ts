8a1e730c9451d5ca5e679de0be173637ac99327e
cubewebs <juanjoseaos@hotmail.com>
2024-08-08 11:22:10 +0100

    Reinstall with omit flag

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize} from "sequelize";
 
-const sequelize = new Sequelize('hospitals_db', 'root', '', {
+const sequelize = new Sequelize(process.env.DB_DATABASE || 'railway', 'root', process.env.DB_PASSWORD || 'dKuvKxKSzQrFGEsrcFmJpLxqdzFwwWFl', {
     host: process.env.DB_HOST || 'mysql.railway.internal',
     port: Number(process.env.DB_PORT) || 3306,
     username: process.env.DB_USER || 'root',
