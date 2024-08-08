747f54e760e1cf16d9d9548bf08eb47a749ad29d
cubewebs <juanjoseaos@hotmail.com>
2024-08-08 13:13:33 +0100

    changed start path

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const sequelize = new sequelize_1.Sequelize(process.env.DB_DATABASE || 'railway', 'root', process.env.DB_PASSWORD || 'dKuvKxKSzQrFGEsrcFmJpLxqdzFwwWFl', {
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 3306,
+    port: Number(process.env.DB_PORT),
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'dKuvKxKSzQrFGEsrcFmJpLxqdzFwwWFl',
     database: process.env.DB_DATABASE || 'railway',
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,7 +2,7 @@ import { Sequelize} from "sequelize";
 
 const sequelize = new Sequelize(process.env.DB_DATABASE || 'railway', 'root', process.env.DB_PASSWORD || 'dKuvKxKSzQrFGEsrcFmJpLxqdzFwwWFl', {
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 3306,
+    port: Number(process.env.DB_PORT),
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'dKuvKxKSzQrFGEsrcFmJpLxqdzFwwWFl',
     database: process.env.DB_DATABASE || 'railway',
