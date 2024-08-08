36ea11060b87171cdcfa6da094125288f3acc935
cubewebs <juanjoseaos@hotmail.com>
2024-08-08 23:57:53 +0100

    port removed

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const sequelize = new sequelize_1.Sequelize('railway', 'root', 'ehqXzgWqwxbfJWleeMHtbKounaSajIkJ', {
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,7 +2,6 @@ import { Sequelize } from "sequelize";
 
 const sequelize: Sequelize = new Sequelize('railway', 'root', 'ehqXzgWqwxbfJWleeMHtbKounaSajIkJ', {
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
