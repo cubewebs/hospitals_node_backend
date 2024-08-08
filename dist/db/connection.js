3372845a390127bbc321d16fa5fa41caa1e3eb2f
cubewebs <juanjoseaos@hotmail.com>
2024-08-08 11:53:39 +0100

    Reinstalled all dependencies

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('hospitals_db', 'root', '', {
+const sequelize = new sequelize_1.Sequelize(process.env.DB_DATABASE || 'railway', 'root', process.env.DB_PASSWORD || 'dKuvKxKSzQrFGEsrcFmJpLxqdzFwwWFl', {
     host: process.env.DB_HOST || 'mysql.railway.internal',
     port: Number(process.env.DB_PORT) || 3306,
     username: process.env.DB_USER || 'root',
