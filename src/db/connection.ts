e9c162e726bd77a209726d76204471edec976e6f
cubewebs <juanjoseaos@hotmail.com>
2024-08-08 11:06:30 +0100

    Added db variables from railway in or hardcode

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,11 +1,11 @@
 import { Sequelize} from "sequelize";
 
 const sequelize = new Sequelize('hospitals_db', 'root', '', {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'mysql.railway.internal',
     port: Number(process.env.DB_PORT) || 3306,
     username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'johnnypassword',
-    database: process.env.DB_DATABASE || 'hospitals_db',
+    password: process.env.DB_PASSWORD || 'dKuvKxKSzQrFGEsrcFmJpLxqdzFwwWFl',
+    database: process.env.DB_DATABASE || 'railway',
     dialect: 'mysql'
 })
 
