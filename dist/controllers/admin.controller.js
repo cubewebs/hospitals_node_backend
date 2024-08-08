61f3e3c11f594b50607ff81b43b0996a04856464
cubewebs <juanjoseaos@hotmail.com>
2024-08-08 11:14:41 +0100

    Deleted node_modules and dist dirs

--- dist/controllers/admin.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAdmin = void 0;
-const getAdmin = (req, res, next) => {
-    res.json('Admin controller');
-};
-exports.getAdmin = getAdmin;
