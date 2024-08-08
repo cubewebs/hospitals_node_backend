1522c7288c03132e765d87fae0004cb49c852036
cubewebs <juanjoseaos@hotmail.com>
2024-08-08 13:21:26 +0100

    changed start path

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize} from "sequelize";
 
-const sequelize = new Sequelize(process.env.DB_DATABASE || 'railway', 'root', process.env.DB_PASSWORD || 'dKuvKxKSzQrFGEsrcFmJpLxqdzFwwWFl', {
+const sequelize = new Sequelize(process.env.DB_DATABASE || 'railway', 'root', {
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     username: process.env.DB_USER || 'root',
